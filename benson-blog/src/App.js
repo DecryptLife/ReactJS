5cdd2b7ad6fb1a5a8f1799ecf3aed0aae08256b1
DecryptLife <benson.thomas2602@gmail.com>
2021-10-08 22:37:52 +0530

    Route Parameters

--- benson-blog/src/App.js
+++ benson-blog/src/App.js
@@ -1,6 +1,8 @@
 import Navbar from "./Navbar";
 import Home from "./Home";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Create from "./Create";
+import BlogDetails from "./BlogDetails";
 
 function App() {
   // const title = "Welcome to the new blog";
@@ -12,8 +14,14 @@ function App() {
         <Navbar />
         <div className="content">
           <Switch>
-            <Route path="/">
+            <Route exact path="/">
               <Home />
+            </Route>
+            <Route path="/create">
+              <Create />
+            </Route>
+            <Route path="/blogs/:id">
+              <BlogDetails />
             </Route>
           </Switch>
         </div>
