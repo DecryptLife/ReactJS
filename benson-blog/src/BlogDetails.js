16b6d8e57ec30162858786b51ed9504f21e92859
DecryptLife <benson.thomas2602@gmail.com>
2021-10-09 12:27:15 +0530

    Redirect & 404

--- benson-blog/src/BlogDetails.js
+++ benson-blog/src/BlogDetails.js
@@ -1,15 +1,22 @@
-import { useParams } from "react-router";
+import { useParams, useHistory } from "react-router";
 import useFetch from "./useFetch";
 
 const BlogDetails = () => {
   const { id } = useParams();
-
+  const history = useHistory();
   const {
     data: blog,
     error,
     isPending,
   } = useFetch("http://localhost:8000/blogs/" + id);
 
+  const handleDelete = () => {
+    fetch("http://localhost:8000/blogs/" + blog.id, {
+      method: "DELETE",
+    }).then(() => {
+      history.push("/");
+    });
+  };
   return (
     <div className="blog-details">
       {/* <h2>Blog Details - {id}</h2> */}
@@ -20,6 +27,7 @@ const BlogDetails = () => {
           <h2>{blog.title}</h2>
           <p>Written by: {blog.author}</p>
           <div>{blog.body}</div>
+          <button onClick={handleDelete}>Delete</button>
         </article>
       )}
     </div>
